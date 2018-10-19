ee5078bf9a7d271fca49b1afbc86cdc671189dec
niugm <niuguomeng@gmail.com>
2018-10-19 14:17:07 +0800

    init

---
+++ pages/about/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div>
+      hello world! <span v-text="name"/>
+    </div>
+    <div>
+      <input
+        v-model="name" 
+        type="text">
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      name: 'niugm'
+    }
+  },
+
+  methods: {
+    changeName(name) {
+      this.name = name
+    }
+  }
+}
+</script>
